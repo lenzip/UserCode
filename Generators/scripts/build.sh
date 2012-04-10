277217b9c0071873b5750df032b74ecad701f196
lenzip <>
2012-04-10 11:38:45 +0000

    update for new cmsBuild syntax

--- Generators/scripts/build.sh
+++ Generators/scripts/build.sh
@@ -6,7 +6,7 @@ export TMPDIR=$BUILD_HOME/tmp
 cd $BUILD_HOME/build
 
 export VO_CMS_SW_DIR=$PWD
-export SCRAM_ARCH=slc5_amd64_gcc434
+export SCRAM_ARCH=slc5_amd64_gcc462
 export LANG C
 
-time ../PKGTOOLS/cmsBuild --cfg ../mydoit.cfg --cmsdist ../CMSDIST 
+time ../PKGTOOLS/cmsBuild -a slc5_amd64_gcc462 --cmsdist ../CMSDIST build rivet professor 
