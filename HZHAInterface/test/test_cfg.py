5d9fcd0b459f2be26c377f1f81e5f74763506995
pjanot <>
2012-04-25 17:01:37 +0000

    Scripts for production

--- HZHAInterface/test/test_cfg.py
+++ HZHAInterface/test/test_cfg.py
@@ -15,14 +15,14 @@ process.generator = cms.EDFilter("HZHAGeneratorFilter",
 #make gen particles and all that stuff
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 process.load("PhysicsTools.HepMCCandAlgos.genParticles_cfi")
-process.load("RecoJets.Configuration.RecoGenJets_cff")
-process.load("RecoMET.Configuration.RecoGenMET_cff")
-process.load("RecoJets.Configuration.GenJetParticles_cff")
-process.load("RecoMET.Configuration.GenMETParticles_cff")
-process.load("RecoJets.JetProducers.ExclusiveNJadeGenJets_cfi") 
-process.load("RecoJets.JetProducers.ExclusiveYcutJadeGenJets_cfi") 
+#process.load("RecoJets.Configuration.RecoGenJets_cff")
+#process.load("RecoMET.Configuration.RecoGenMET_cff")
+#process.load("RecoJets.Configuration.GenJetParticles_cff")
+#process.load("RecoMET.Configuration.GenMETParticles_cff")
+#process.load("RecoJets.JetProducers.ExclusiveNJadeGenJets_cfi") 
+#process.load("RecoJets.JetProducers.ExclusiveYcutJadeGenJets_cfi") 
 #process.genJetMET = cms.Sequence(process.genJetParticles*process.recoGenJets+process.genMETParticles*process.recoGenMET)
-process.genJetMET = cms.Sequence(process.genJetParticles*process.ExclusiveNJadeGenJets+process.ExclusiveYcutJadeGenJets)
+#process.genJetMET = cms.Sequence(process.genJetParticles*process.ExclusiveNJadeGenJets+process.ExclusiveYcutJadeGenJets)
 
 #ourtput module
 process.GEN = cms.OutputModule("PoolOutputModule",
@@ -31,6 +31,7 @@ process.GEN = cms.OutputModule("PoolOutputModule",
 
 #paths
 process.p = cms.Path(process.generator+
-                    process.genParticles+
-                    process.genJetMET)
+                    process.genParticles#+
+                    #process.genJetMET
+                     )
 process.outpath = cms.EndPath(process.GEN)
